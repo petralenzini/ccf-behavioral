706ca7dedbdcd82d200784168d53b2397fb6a1cb
Moises Baltazar <me@moisesb.com>
2020-01-18 10:24:02 -0600

    Remove ability to pass credentials via arguments
    Since we're moving towards a jupyter notebook, passing plain-text credentials
    will store those secrets in the notebook if the notebook is hosted on
    github and this could lead to data breach.

--- src/config.py
+++ src/config.py
@@ -11,45 +11,25 @@ def load_yaml(filename):
     with open(filename, 'r') as fd:
         return yaml.load(fd, Loader=yaml.SafeLoader)
 
+
+def recursive_update(old, new):
+    if old is not None and type(old) is dict and type(new) is dict:
+        old = old.copy()
+        for k, v in new.items():
+            old[k] = recursive_update(old.get(k), v)
+        return old
+
+    else:
+        return new
+
+
 def LoadSettings(useArgs=False):
     config = load_yaml('./config.yml')
 
-    if 'credentials' in config:
-        config.update(load_yaml(config['credentials']))
+    if 'secrets' in config:
+        secrets = load_yaml(config['secrets'])
+        config = recursive_update(config, secrets)
 
-    if useArgs:
-        config = __load_args__(config)
-
-    return config
-
-
-def __load_args__(config):
-    parser = argparse.ArgumentParser(description="Download the data")
-
-    user_required = 'user' not in config
-    user_group = parser.add_mutually_exclusive_group(required=user_required)
-    user_group.add_argument("-u", "--user", type=str, help="username")
-    user_group.add_argument("-U", "--userexec", metavar="EXEC", type=str, help="run command to get username")
-
-    pass_required = 'user' not in config
-    password_group = parser.add_mutually_exclusive_group(required=pass_required)
-    password_group.add_argument("-p", "--password", type=str, help="password")
-    password_group.add_argument("-P", "--passwordexec", metavar="EXEC", type=str, help="run command to get password")
-
-    args = parser.parse_args()
-    user = subprocess.check_output(args.userexec, shell=True).decode() \
-        if args.userexec else \
-        args.user
-
-    password = subprocess.check_output(args.passwordexec, shell=True).decode() \
-        if args.passwordexec else \
-        args.password
-
-    if user:
-        config['user'] = user.strip()
-
-    if password:
-        config['password'] = password.strip()
 
     return config
 
