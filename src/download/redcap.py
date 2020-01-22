c34f961b7de57d15278944cdc0949d7ebb31b7cc
Moises Baltazar <me@moisesb.com>
2020-01-22 09:20:47 -0600

    Move functions into Redcap class
    They rely on a variable which used to be in the global scope but is now
    class-scoped. Therefore, to access the variable, they too must be
    class-scoped.

--- src/download/redcap.py
+++ src/download/redcap.py
@@ -11,64 +11,67 @@ red = config['tables']
 default_url = config['api_url']
 
 
-def redcapApi(token, fields=[], events=[],
-              format='csv',
-              content='record',
-              type='flat',
-              returnFormat='json',
-              rawOrLabel='raw',
-              rawOrLabelHeaders='raw',
-              exportCheckboxLabel='false',
-              exportSurveyFields='false',
-              exportDataAccessGroups='false'):
-    data = {
-        'token': token,
-        'format': format,
-        'content': content,
-        'type': type,
-        'returnFormat': returnFormat,
-        'rawOrLabel': rawOrLabel,
-        'rawOrLabelHeaders': rawOrLabelHeaders,
-        'exportCheckboxLabel': exportCheckboxLabel,
-        'exportSurveyFields': exportSurveyFields,
-        'exportDataAccessGroups': exportDataAccessGroups,
-    }
-    if fields:
-        data['fields[]'] = fields
-
-    if events:
-        data['events[]'] = events
-
-    r = requests.post(default_url, data=data)
-    r = io.BytesIO(r.content)
-    return pd.read_csv(r, encoding='utf8')
-
-
-def get_all_rows(study, fieldlist=[], subjectOnly=False):
-    s = red[study]
-    fieldnames = s['fields']
-    token = s['token']
-    events = [s['events']]
-    if subjectOnly:
-        fields = [fieldnames['field']]
-    else:
-        fields = list(fieldnames.values())
-    fields.extend(fieldlist)
-
-    df = redcapApi(token, fields, events)
-    df.rename(columns={fieldnames['interview_date']: 'interview_date'}, inplace=True)
-    df = df[df[fieldnames['field']] != '']
-    split_df = df[fieldnames['field']].str.split("_", 1, expand=True)
-    df['subject'] = split_df[0].str.strip()
-    df['flagged'] = split_df[1].str.strip()
-    df['study'] = study
-
-    return df
 
 
 class Redcap:
-    def __init__(self):
-        pass
+    def __init__(self, url=default_url):
+        self.url = url
+
+    def redcapApi(self, token, fields=[], events=[], forms=[],
+                  format='csv',
+                  content='record',
+                  type='flat',
+                  returnFormat='json',
+                  rawOrLabel='raw',
+                  rawOrLabelHeaders='raw',
+                  exportCheckboxLabel='false',
+                  exportSurveyFields='false',
+                  exportDataAccessGroups='false'):
+        data = {
+            'token': token,
+            'format': format,
+            'content': content,
+            'type': type,
+            'returnFormat': returnFormat,
+            'rawOrLabel': rawOrLabel,
+            'rawOrLabelHeaders': rawOrLabelHeaders,
+            'exportCheckboxLabel': exportCheckboxLabel,
+            'exportSurveyFields': exportSurveyFields,
+            'exportDataAccessGroups': exportDataAccessGroups,
+        }
+        if fields:
+            data['fields[]'] = fields
+
+        if events:
+            data['events[]'] = events
+
+        if forms:
+            data['forms[]'] = forms
+
+        r = requests.post(self.url, data=data)
+        r = io.BytesIO(r.content)
+        return pd.read_csv(r, encoding='utf8', parse_dates=True)
+
+    def get_all_rows(self, study, fieldlist=[], subjectOnly=False):
+        s = red[study]
+        fieldnames = s['fields']
+        token = s['token']
+        events = [s['events']]
+        if subjectOnly:
+            fields = [fieldnames['field']]
+        else:
+            fields = list(fieldnames.values())
+        fields.extend(fieldlist)
+
+        df = self.redcapApi(token, fields, events)
+        df.rename(columns={fieldnames['interview_date']: 'interview_date'}, inplace=True)
+        df = df[df[fieldnames['field']] != '']
+        split_df = df[fieldnames['field']].str.split("_", 1, expand=True)
+        df['subject'] = split_df[0].str.strip()
+        df['flagged'] = split_df[1].str.strip()
+        df['study'] = study
+
+        return df
 
     def getredcapdata(self, fulldata=False):
         """
@@ -88,7 +91,7 @@ class Redcap:
             # skip 1st row of auth which holds parent info
             fields_dict.remove('hcpdparent')
 
-        return pd.concat([get_all_rows(name) for name in fields_dict], sort=True)
+        return pd.concat([self.get_all_rows(name) for name in fields_dict], sort=True)
 
     def getfullredcapdata(self):
         return self.getredcapdata(True)
@@ -102,7 +105,7 @@ class Redcap:
         flagged contains the extra characters other than the id so you can keep track of who should NOT be uploaded to NDA
         or elsewwhere shared
         """
-        studydata = get_all_rows(study, fieldlist)
+        studydata = self.get_all_rows(study, fieldlist)
         # Convert age in years to age in months
         # note that dob is hardcoded var name here because all redcap databases use same variable name...sue me
         # interview date, which was originally v1_date for hcpa, has been
@@ -137,8 +140,8 @@ class Redcap:
         studyids = pd.DataFrame()
 
         for name in red.keys():
-            df = get_all_rows(name, subjectOnly=True)
-            df.columns = ['Subject_ID', 'subject', 'flagged', 'study']
+            df = self.get_all_rows(name, subjectOnly=True)
+            df.columns = ['subject_id', 'subject', 'flagged', 'study']
             studyids = studyids.append(df)
 
         return studyids
